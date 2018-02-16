8c2d826fb1f59b8dfcb0033e0da00257f2b64716
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-02-16 11:06:34 -0800

    updated config

--- src/main.js
+++ src/main.js
@@ -6,20 +6,23 @@ import AWSAppSyncClient from "aws-appsync"
 import VueApollo from 'vue-apollo'
 import appSyncConfig from './AppSync'
 
-const client = new AWSAppSyncClient({
+const config = {
   url: appSyncConfig.graphqlEndpoint,
   region: appSyncConfig.region,
   auth: {
     type: appSyncConfig.authenticationType,
     apiKey: appSyncConfig.apiKey,
   }
-},{
+}
+const options = {
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'cache-and-network',
     }
   }
-})
+}
+
+const client = new AWSAppSyncClient(config, options)
 
 const appsyncProvider = new VueApollo({
   defaultClient: client
