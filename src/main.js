ce276dab68b66d10c4a3adfaf2f65f7d009a92e7
Brian Kinney <webdevbrian@gmail.com>
2018-02-27 14:52:58 -0500

    Add required set immediate to package json

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import setimmediate from 'setimmediate'
 import AWSAppSyncClient from "aws-appsync"
 import VueApollo from 'vue-apollo'
 import appSyncConfig from './AppSync'
